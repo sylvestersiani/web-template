4ec86e1af57dbdaca15c4fad8c8489a83261625e
Sylvester <contact@sylvestersiani.co.uk>
2017-03-20 22:03:29 +0000

    trustees page

--- public/inc/pages/trustees.php
+++ public/inc/pages/trustees.php
@@ -2,29 +2,53 @@
 <style type="text/css">
 	.trustees{
 		
-		height: 300px;
+		height: 350px;
 		background-color: black;
 		color: #fff;
 		margin-top: 20px;
 		border-radius: 5px;
-		padding: 20px;
+
 	}
 
 .trustees > div{
 	margin: auto;
 	width: 90%;
+	padding: 20px;
+	
 }
 
 .trustess-details{
 	width: 60%;
 	float: left;
 	margin-right: 30px;
+	overflow: none;
 }
-.trustees  img{
+
+.trustess-details > h1{
+	margin-bottom: 20px;
+}
+
+.trustees img{
 	float: right;
 	width:auto;
 	height: 250px;
 }
+
+@media (max-width: 800px) {
+	.trustees {
+		height: auto;
+	}
+	.trustess-details{
+		width: 100%;
+		float: none;
+	}
+	.trustees img{
+		width: 100%;
+		height: auto;
+		float: none;
+	}
+}
+
 
 </style>
 
@@ -32,17 +56,26 @@
 
 
 <?php 
-
-
-
-function trustees($name, $image="#"){
+function trustees($name, $title, $image="#", $text=''){
 ?>
-
 	<div class="trustees">
 		<div>
 			<div class="trustess-details">
-				<h1><?php isset($name)? print($name): 'staff'; ?></h1>
-				<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+				<h1>
+					<?php 
+						if (isset($name) && isset($title)) {
+							echo $name . ' - ' . $title;
+						}else{echo 'Staff';}
+					?>	
+				</h1>
+				<p>
+					<?php 
+						if (isset($text)) {
+							echo wordwrap($text, 20);
+						}else{echo '';}
+					?>
+				</p>
+
 			</div>
 			<img src="<?php isset($image)? print($image): '#'; ?>">
 		</div>
@@ -57,12 +90,21 @@ function trustees($name, $image="#"){
 
 
 <?php 
-	trustees('Dalie', 'https://gm1.ggpht.com/90d38OwZt_BwFmMqaT6QOiQEyDAhO-Ji_GSZFUX3AKWqp9tblaHDUgOb5h1yfRDVDqAeUPaRT12CSA8PzmssSZ1U0UCyiY1rLTLLLraH49BCThT-9IgDpjactem40JTHZ9ChtMqpRAWbBN2rWzybnZ76fLTdfEE0dvmq_Aqi5vBNhz9ugIkPBBA2t5vM6GT1qgKKvFpYa6oPaL_MnMiQiRhjGzuvb7l5gNv9Z98dAhpm6TMvA1tfeEu8wigiDijUHJBMlb6nQbtf6536Ua4s975C7ibMG_ASkR9X0BmZ_0p8utgujOzyNOeui46VFIPZxbQoaUR32VlzQudeLbNnTSrWOFBo5HzCPRQOJo5kMyYFp1ZbQbPUl-p0lep7Ex1fNC1WD4AFp3XeL7N_0pybskbagtnAJlIEVbnGW5KFyhzUtKL60gk2zDzYLTJmHhaW5n4gtlWEQJWFMfGHjqS3J089aZ5blPbHVhpk6O_L7xXf_zsrq0NM01RE4_hJvf1WiuW0BjFW9bKMdwcnK7zj1K494WSYzVzg_jXfEn3u15lQbELEirhuwliWoutKVznE1ocytIl2Y7h3Nf3rweRuO1nRfD9hc2RsLU1guag1iuRiLJA9BrAlguWfxk3a_cC-F6hOdxN1Pfx4K4lMzyFuqmRTppS9jQtUhzrt7cIOepOM1S_gBbuXsjeV_0C0bfS8AA1AVz50MMbtIhAi1UTc7m2hbXE=w2358-h1138-l75-ft');
-
-	trustees('Michael', 'https://mail.google.com/mail/u/1/?ui=2&ik=27dd3c904c&view=fimg&th=15ae77fe208911b7&attid=0.2&disp=inline&realattid=f_j0gvvtrs1&safe=1&attbid=ANGjdJ8ekJNd9X5iJ4iguVcXsjbxptx_slaB-0PfbtTWyolc550cWRcsew0V0SBY8TOswOesSL-4b2xaqauLOBY1pcPOkwvFsgFzPnz0MOH4WRTR_yHhECEQclGQhCU&ats=1489977611506&rm=15ae77fe208911b7&zw&sz=w2358-h1138');
-
-	trustees('Staff 3', 'https://gm1.ggpht.com/s5nWD4llrUsgQclfgLnxWr_Ct6P4bzNTKiRsaLGJCEvZ-I7URM_HGmO5b27myd4qSAAEUSWIO8X4VKDfWXCVPG4X_m2ZUwFRc720bII6XV4J78H0xXmqT0xZu7aBgkqQlNS4CKgQoZBaToX1gwXqFudbBzZQVsFYPvv4TKdxp_88XI-DCb_cjVuU8zCQ0dw4nv8164UPQ6nRarwogoEw3kbK11_XELxabwgSuRkmMpzkOdz4TCa5d4mNUZnGyLn2AuZw9BqSQo62hLuAkXEeMHRMSP3-7pSjTCwr2Db0_eFbpsT6WLBCpZNYTmMP0QJRlMhGxYIaIs77YzXrZQkxmpRiWTUFsMwVTJpSN23m77HSoSFv7DZXgycq1IRgqUCZYxh5zPUJIbLF9a8RnTohxqItSljlnrmUlbOWw76tcqSwl2LrQ2agra9rq7yKSlfhGnhhdidVkHBJpvrOyaNnTHpPVYpw2bu2el3Y-DwV2kKS_7zKg8MDzh2rT1ISLaQoN4yuYBPh5g-8f-fZr63th7q7TC9LtSFHQKtg-kQnsTzsNEDMq_sSSWyiUEkmEHBSjMffLNu5KnaXTzySMXbA9OiFxS28xJhaw9qzzxqD8Am9fmqVO6Y6llBhL2WlqFanH_sHZot4V0sIzObByZX8OEcN94xpsquZOnorAZmx4oIVZycc8BylMp4j8yka8aTLjhy8sepBO2YHR_0gFSDCBkNv7SSu7A=w2358-h1138-l75-ft');
+	trustees('Dalie', 'Co-Founder &amp; CEO', 'public/img/dalie.jpg','Dalie is an education professional with a background in the voluntary sector.  With a degree in International Relations, Dalie has always been passionate about societal development, especially in the Third World, and equal opportunities for all.
+In 2015, Dalie’s father fell ill and was eventually diagnosed with Alzheimer’s disease. Dalie was forced to become her father’s full-time Carer for 9 months and experienced first-hand the challenges caused by sudden mental illness in a person’s life. With mental disease being somewhat of a foreign subject within Congolese circles, Dalie and her immediate family faced a lot of prejudice and stigma from their community, and consequently had to hide their plight. This led Dalie to embarked on a journey to start up an organisation which would essentially eradicate this stigma, and help towards supporting mental health sufferers and their families within African communities, to live supportive lives, free of stigma or social bias.
+Dalie firmly believes that no illness or disability should be viewed with so much disdain that sufferers do not reach out for help.  Dalie also has a firm belief in the power of education and arts, and the ability these factors have in changing mindsets and perceptions of people - hence the inception of Kwetu.  Moreover, as an advocate for social justice, Dalie feels that her ultimate purpose in life is to bridge gaps of inequality within society.' );
 
 
+	trustees('Lydia Mumba', 'Co-Founder & Trustee', 'public/img/lydia.jpg', 'After having gone through challenging times as a result of her father falling ill with Alzheimer’s dementia, Lydia Mumba joined forces with her sister to found Kwetu.
+Lydia has a degree in Psychology from the University of Roehampton and is particularly interested in relationship building.  Aside from her day job at the Nursing & Mid-wifery council, Lydia dedicates much of her time to building networks and promoting organisations doing great things.
+With much insight on social media marketing, Lydia currently manages Kwetu’s social media presence.
+');
+
+
+
+trustees('Michael Otadenede', 'Trustee & Acting Chair', 'public/img/michael.jpg', 'Michael has considerable experience in corporate social responsibility, and has developed his expertise primarily designing and delivering community affairs programmes for large scale corporates and third sector organisations. His approach centres on stakeholder engagement – both internally and externally – and project management – from strategy to delivery & through to impact measurement.
+Michael is also the Co-founder of Young Professional Congolese (YPC), the first UK based network for young Congolese professionals, which aims to build a visible and active network of young professional Congolese across the UK. 
+With a passion towards community affairs and societal development, Michael’s reputation within the Congolese community (as an African subgroup) as well as his experience in setting up and establishing a community-focused start-up will prove pivotal in the growth here at Kwetu.
+');
 ?>
 
