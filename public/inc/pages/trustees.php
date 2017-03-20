acedb318a9a825232aa611dbd0bd868e99d639b9
Sylvester <contact@sylvestersiani.co.uk>
2017-03-20 02:29:30 +0000

    trustees page

---
+++ public/inc/pages/trustees.php
@@ -0,0 +1,39 @@
+
+<style type="text/css">
+	.trustees{
+		
+		height: 300px;
+		background-color: black;
+		color: #fff;
+		margin-top: 20px;
+		border-radius: 5px;
+		padding: 20px;
+	}
+
+.trustees > div{
+	margin: auto;
+	width: 90%;
+}
+
+.trustess-details{
+	width: 60%;
+	float: left;
+	margin-right: 30px;
+}
+.trustess  > img{
+	float: right;
+	width: 150px;
+	height: 150px;
+}
+
+</style>
+
+<div class="trustees">
+	<div>
+	<div class="trustess-details">
+		<h1>Name</h1>
+		<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+	</div>
+	<img src="https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=300&h=300">
+	</div>
+</div>
