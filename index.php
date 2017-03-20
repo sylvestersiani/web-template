cf730ab80b10a68fd8a38fc2ae2f596ef64b0cab
Sylvester <contact@sylvestersiani.co.uk>
2017-03-20 01:52:20 +0000

    switch
    adding fun to home page

--- index.php
+++ index.php
@@ -9,8 +9,11 @@
 		if (isset($_GET['page'])) {
 			$page = $_GET['page'];
 
+
 			switch ($page) {
 				case 'about':
+					background_img("http://africa-facts.org/wp-content/uploads/2015/01/african-kids.jpg");
+
 					include 'public/inc/pages/about.php';
 					break;
 				case 'index':
