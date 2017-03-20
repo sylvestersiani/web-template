1de44347d9cae6cc5058c662f220528e2d63df67
Sylvester <contact@sylvestersiani.co.uk>
2017-03-20 01:51:53 +0000

    image change
    updating image background using php

--- controller/func.php
+++ controller/func.php
@@ -13,3 +13,8 @@ function render_template($template){
 function render_page($page, $title='template'){
 			require_once 'public/inc/pages/'. $page.'.php';
 		}
+
+/// changing background image
+	function background_img($link){
+				echo "<style> body{background: url(". $link. ")} </style>";
+			}
