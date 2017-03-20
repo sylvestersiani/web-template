12bf58d445a54bba66f2fa2a084ff13992b3def1
Sylvester <contact@sylvestersiani.co.uk>
2017-03-20 01:35:11 +0000

    Revert "home"
    This reverts commit 92574e6b09d9f51458c03694d4387bc0f526a224.

--- public/inc/pages/about.php
+++ public/inc/pages/about.php
@@ -1,4 +1,32 @@
 
+<style type="text/css">
+	.text-segment{
+		background-color: #fff;
+		border-radius: 5px;
+		-moz-border-radius: 20px;
+		-webkit-border-radius: 20px;
+		width: 450px;
+		float: right;
+		line-height: 1.5em;
+		font-weight: 300;
+		opacity: 0.8;
+		margin-top: 10px;
+		margin-right: 40px;
+	}
+	.text-segment div{
+		padding: 20px;
+	}
+
+
+	#about-us{
+		min-width: 100%;
+		height: 100%;
+		background:url("http://africa-facts.org/wp-content/uploads/2015/01/african-kids.jpg");
+
+	}
+
+
+</style>
 
 
 
