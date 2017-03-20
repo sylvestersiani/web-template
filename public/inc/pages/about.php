54f226fb69c260b58f930236552db5c06dfc58a0
Sylvester <contact@sylvestersiani.co.uk>
2017-03-20 01:11:53 +0000

    about us page

--- public/inc/pages/about.php
+++ public/inc/pages/about.php
@@ -1,16 +1,45 @@
 
 <style type="text/css">
-	
+	.text-segment{
+		background-color: #fff;
+		border-radius: 5px;
+		-moz-border-radius: 20px;
+		-webkit-border-radius: 20px;
+		width: 450px;
+		float: right;
+		line-height: 1.5em;
+		font-weight: 300;
+		opacity: 0.8;
+		margin-top: 10px;
+		margin-right: 40px;
+	}
+	.text-segment div{
+		padding: 20px;
+	}
+
+
+	#about-us{
+		min-width: 100%;
+		height: 100%;
+		background:url("http://africa-facts.org/wp-content/uploads/2015/01/african-kids.jpg");
+
+	}
+
 
 </style>
 
 
 
-<div class="">
+<div id="about-us">
 	<div class="text-segment">
+		<div>
 		<h1>Our Story</h1>
 		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.
 		It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
 		It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+		It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+		It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+		</div>
 	</div>
 </div>
