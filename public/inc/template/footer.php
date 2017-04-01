12eee5d3a4695e58fa1889e925b191a8fefced1c
crtndrmr <plopes.sw@gmail.com>
2017-04-01 02:47:50 +0100

    Footer links

--- public/inc/template/footer.php
+++ public/inc/template/footer.php
@@ -1,7 +1,23 @@
-</div>
+
 <footer id="footer">
-	
+	<div class="copyright">
+		<p>Copyright 2017 NAME</p>
+	</div>
+	<div class="directory">
+		<ul>
+			<li><a href="<?php echo "index.php?page=". htmlspecialchars("index")?>">accessibility</a></li>
+
+			<li><a href="<?php echo "index.php?page=". htmlspecialchars("about")?>">contact Us</a></li>
+
+			<li><a href="<?php echo "index.php?page=". htmlspecialchars("get-involved")?>">privacy</a></li>
+
+			<li><a href="<?php echo "index.php?page=". htmlspecialchars("trustees")?>">policy</a></li>
+
+			<li><a href="<?php echo "index.php?page=". htmlspecialchars("trustees")?>">sitemap</a></li>
+		</ul>
+	</div>
 </footer>
+</div>
 <script>
 $(document).ready(function(){
     $(".trigger").click(function(){
