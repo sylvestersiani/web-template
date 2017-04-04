830af6adca408c25097a7897e1e66231446ecdb4
Sylvester <contact@sylvestersiani.co.uk>
2017-04-04 22:52:22 +0100

    home page div

--- public/inc/pages/home.php
+++ public/inc/pages/home.php
@@ -1,12 +1,25 @@
-<style type="text/css">
-	.landing-page{
-		min-height: 100%;
-	}
 
-</style>
 
 <div class="landing-page">
 	<h2> 
-		Serving the african comunity and other minority groups by inspiring and motivating a revolutionary change in the way mental illness is percieved. We aim to connect minds and support families battling stigmas of mental health. 
+		"Serving the african comunity and other minority groups by inspiring and motivating a revolutionary change in the way mental illness is percieved.
+		We aim to connect minds and support families battling stigmas of mental health." 
 	</h2>
+</div>
+
+<div class="landing-segment2">
+	<div class="notice">
+		<h4>We are recruiting for Trustees!!</h4>
+		<p>
+		We are seeking candidates who are as passionate about mental health within the black community as we are, who have great creativity and strategic thinking, who have the right networks, and who will ultimately help steer the organisation in achieving its core aims.
+		</p>
+	</div>
+	<div class="newsletter">
+		<h4>Stay Informed</h4>
+		<p>Enter your email to receive relevant messaging tips  and examples. <br> You can unsubscribe at any time.</p>
+		<form action="GET" method="" >
+			<input type="email" name="email" placeholder="Enter your email">
+			<button type="submit">Submit</button>
+		</form>
+	</div>
 </div>
