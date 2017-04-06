6135c471c34d47573ed23e73af719f5c181afa27
Sylvester <contact@sylvestersiani.co.uk>
2017-04-06 10:30:36 +0100

    contact form back end

--- public/inc/pages/contact.php
+++ public/inc/pages/contact.php
@@ -1,11 +1,13 @@
 
+<?php include 'controller/contact-exec.php'; ?>
 <div id="form">
-	<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+	<form method="post" action="controller/contact-exec.php">
 		<ul id="form-opt">
 			<li><input type="name" name="name" placeholder="NAME" ></li>
 			<li><input type="email" name="email" placeholder="EMAIL"></li>
 			<li><input type="number" name="number" placeholder="PHONE NUMBER"></li>
 			<li><textarea type="text" placeholder="..."></textarea></li>
+			<li><input type="submit" name="submit"></li>
 		</ul>
 	</form>
 </div
